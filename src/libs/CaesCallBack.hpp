89eb70c5290e0afe762de63738e47476e6f452be
catraeus <duncanbranch@gmail.com>
2022-02-19 20:01:17 -0600

    tweak

--- src/libs/CaesCallBack.hpp
+++ src/libs/CaesCallBack.hpp
@@ -1,34 +1,53 @@
+// $Id: CaesCallBack.hpp 497 2019-08-17 20:46:44Z duncang $
 
-// $Id: CaesCallBack.hpp 141 2017-06-02 01:39:10Z duncang $
-//  Callback.hpp
+//=================================================================================================
+// Original File Name : CaesCallBack.hpp
+// Original Author    : duncang
+// Creation Date      : Oct 14, 2012
+// Copyright          : Copyright © 2012 - 2022 by Catraeus and Duncan Gray
+//
+// Description        :
+/*
+  The whole point of callbacks is to invert the normal hierarchy of caller/called.  NOW, the called
+    is above and the caller is in the "included" module of the called function.  Thus include hell
+    doesn't happen.
+  The magic of the present method is that the base CB class, which is put into the lower file,
+    doesn't have the templating.  Therefore, it doesn't force the lower-level calling Member
+    Function that will invoke the CB to need the upper level caller's class definition.
+    No DANGEROUS Forward Referencing.
+  Finally, since this is c++, the deadly function-pointer-to-a-member-function conundrum isn't
+    raising its poisonous little head.
+  The term "upper file" means the one that has the function to be called back and includes the
+    "lower file's" header.
+  The "lower file," duh, is the one that got included in the upper file and will execute the
+    callback.
+  The void pointer in the functor pattern allows for the knowledge-share so that is a signature
+    that both the lower and upper must by MAGICK be same.
+  1.  Put the pointer to CbV into the lower file as a public member variable.  It won't be
+        "new'd" only set from above.
+  2.  NULL out the pointer to the CbV in the lower file in its constructor.
+  3.  Put a pointer to the CbT in the includer file's own header file "above."
+        With the templating of its own self class.
+        new it.
+  4.  Make a function in the upper file that has a bool return and a void * input parameter.
+  5.  "Set" the callback in the upper file with the functor/functee pattern.
+  6.  Assign the public member variable of the lower file to the functor/functee from the upper file.
+  7.  Execute() 'till the cows come home.
+*/
+//
+//=================================================================================================
+
 
 #ifdef _FIND_BADNESS_
 #define PRINT_INOUT(msg) fprintf(stdout, msg); fflush(stdout);
 #else
-#define PRINT_INOUT(msg)
+#define PRINT_INOUT(msg) (void)(msg)
 #endif
 
 #ifndef __CAEV_CALLBACK_HPP_
 #define __CAEV_CALLBACK_HPP_
 #include <stdio.h>
-/* \cond HIDDEN_SYMBOLS */
-/*=====================================================================================================================
-  The magic of this is that the base CB class doesn't have the templating.  Therefore, it doesn't force the
-    lower-level called Member Function that will invoke the CB to need the upper level caller's class definition.
 
-  I hereby declare two new C++ (maybe even object oriented) keywords:
-    - Helper Callback
-        A callback that is pushed, at call time, by a parent class into a contained class' Member Function as a
-            Parameter so that the contained class doesn't have to have a Member Callback Field that is pre-set.
-              Of course a contained class could have a member callback that gets set in the working called member
-              function of the contained class, but I have always believed it to be sloppy functionality to have
-              Operational Member Functions change its Setup Member Fields at the whim of operations.  The volatility
-              of Member Fields is what i'm talking about here.  An int field that does fast work needs to be updated
-              at every Operation Member Function call.  A Callback Member Field probably shouldn't be that volatile.
-    - Contributor Callback
-        A callback that is set, early in the life of a lower level class and only changes infrequently.  It is a Member
-        Field of the lower level class.  It has things like Set, Get and Clear functions to hide it.
-=======================================================================================================================*/
                     class CbV {
   public:
     virtual      ~CbV() {} // to keep compilers happy, is a trivial destructor that doesn't reduce pure virtuality.
@@ -39,16 +58,16 @@ template <class tI> class CbT : public CbV {
     typedef bool (tI::*tF)(void *i_object); // funny ha ha lessor/lessee <=> functor/functee
   public:
                   CbT()  {
-      //fprintf(stdout, "CbT::CbT()\n"); fflush(stdout);
+      PRINT_INOUT("CbT::CbT()\n");
       pF = 0; // Important : zeroing the pointer so we later can check for errors
       pI = 0;
       }
                  ~CbT() {
-    //fprintf(stdout, "CbT::~CbT()\n"); fflush(stdout);
+    PRINT_INOUT("CbT::~CbT()\n");
     }
     virtual bool  Execute(void *i_object) const {
       if (pF != 0)   return (pI->*pF)(i_object);
-      else           PRINT_INOUT("  **  ERROR ** Uninitialized functionality in CbT\n")
+      else           fprintf(stdout, "  **  ERROR ** Uninitialized functionality in CbT\n");
       return false;
       }
             void  SetCallback (tI  *i_pI, tF   i_pF) {
@@ -63,7 +82,7 @@ template <class tI> class CbT : public CbV {
     tI  *pI; // The Instance that got templated down into us.
     tF   pF; // The function that the instance contains that got pointered down into us.
   };
-/* \endcond HIDDEN_SYMBOLS */
+
 #endif // __CAEV_CALLBACK_HPP_
 
 
